0b2c0a895858c4704905b42b8934010ade4d02c9
Jesper Andersen <jesp727i@edu.eal.dk>
2017-11-30 21:54:51 +0100

    Add project files.

---
+++ BuisnessLayer/Broadcast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLayer
+{
+    //jesper er en engel
+    class Broadcast
+    {
+        static List<ClientHandler> clientList = new List<ClientHandler>();
+
+
+
+        static public void addClientToList(ClientHandler client)
+        {
+            clientList.Add(client);
+        }
+        static public void BroadcastToClients(string messsageToAll)
+        {
+            foreach (ClientHandler client in clientList)
+            {
+                client.SendToClient(messsageToAll);
+            }
+        }
+    }
+}
---
+++ BuisnessLayer/ClientHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+
+namespace BuisnessLayer
+{
+    public class ClientHandler
+    {
+        Thread clientThread;
+        TcpClient newClient;
+
+        NetworkStream nWS;
+        StreamReader sR;
+        StreamWriter sW;
+        string Name;
+
+        public ClientHandler(TcpClient newClient)
+        {
+            this.newClient = newClient;
+            nWS = newClient.GetStream();
+            sR = new StreamReader(nWS);
+            sW = new StreamWriter(nWS);
+            StartClient();
+        }
+
+        public void StartClient()
+        {
+            SendToClient("Indtast Navn:");
+            Name = sR.ReadLine();
+            SendToClient("Velkommen " + Name);
+            clientThread = new Thread(HandelClient);
+            clientThread.Start();
+        }
+
+        public void HandelClient()
+        {
+            string text;
+            while (true)
+            {
+                text = sR.ReadLine();
+                Console.WriteLine(text);
+                Broadcast.BroadcastToClients(Name + " har budt " + text);
+            }
+        }
+        public void SendToClient(string messageToClient)
+        {
+            sW.WriteLine(messageToClient);
+            sW.Flush();
+
+        }
+    }
+}
